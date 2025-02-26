1e4df1bfe7c925ef45f18281444c7b9aae10910b
Kamil Nowowiejski <kamil.nowowiejski.jez@gmail.com>
2025-02-26 16:02:36 +0100

    Commands for sharing and unsharing clipboard

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -3,3 +3,9 @@ vim.api.nvim_create_user_command("Dcmp", "lua require('cmp').setup.buffer {enabl
 
 -- Enable auto-completion for buffer
 vim.api.nvim_create_user_command("Ecmp", "lua require('cmp').setup.buffer {enabled = true}", {})
+
+-- Share clipboard
+vim.api.nvim_create_user_command("Sc", "set cb=unnamedplus | echo 'Clipboard shared'", {})
+
+-- Unshare clipboard
+vim.api.nvim_create_user_command("Usc", "set cb= | echo 'Clipboard unshared'", {})
