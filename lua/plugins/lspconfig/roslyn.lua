90d27ac46133fb5c88d16e5b5cc39decb2fbdf55
Kamil Nowowiejski <kamil.nowowiejski.jez@gmail.com>
2025-04-13 10:25:00 +0200

    fixed path for roslyn

--- lua/plugins/lspconfig/roslyn.lua
+++ lua/plugins/lspconfig/roslyn.lua
@@ -3,5 +3,9 @@ return {
     ft = "cs",
     opts = {
         -- your configuration comes here; leave empty for default settings
+        exe = {
+            "dotnet",
+            vim.fs.joinpath(vim.fn.stdpath("data"), "roslyn", "Microsoft.CodeAnalysis.LanguageServer.dll"),
+        },
     }
 }
