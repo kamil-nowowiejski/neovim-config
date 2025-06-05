2118be310718b0caf22dd07758af10c3f4544bb7
kamil-nowowiejski <kamel6368@gmail.com>
2025-06-05 16:21:41 +0200

    setup new roslyn

--- lua/plugins/lspconfig/init.lua
+++ lua/plugins/lspconfig/init.lua
@@ -16,6 +16,13 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 	},
 	config = function()
+        require("mason").setup({
+            registries = {
+                "github:mason-org/mason-registry",
+                "github:Crashdummyy/mason-registry",
+            },
+        })
+
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 			callback = function(event)
--- lua/plugins/lspconfig/roslyn.lua
+++ lua/plugins/lspconfig/roslyn.lua
@@ -1,11 +1,23 @@
 return {
     "seblyng/roslyn.nvim",
-    ft = "cs",
-    opts = {
-        -- your configuration comes here; leave empty for default settings
-        exe = {
-            "dotnet",
-            vim.fs.joinpath(vim.fn.stdpath("data"), "roslyn", "Microsoft.CodeAnalysis.LanguageServer.dll"),
-        },
-    }
+    -- ft = "cs",
+    -- opts = {
+    --     -- your configuration comes here; leave empty for default settings
+    --     exe = {
+    --         "dotnet",
+    --         vim.fs.joinpath(vim.fn.stdpath("data"), "roslyn", "Microsoft.CodeAnalysis.LanguageServer.dll"),
+    --     },
+    -- }
+    config = function ()
+        require('roslyn').setup()
+    --     vim.lsp.config("roslyn", {
+    --         cmd = {
+    --             "dotnet",
+    --             vim.fs.joinpath(vim.fn.stdpath("data"), "roslyn", "Microsoft.CodeAnalysis.LanguageServer.dll"),
+    --             "--logLevel=Information",
+    --             "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
+    --             "--stdio",
+    --         },
+    --     })
+    end
 }
